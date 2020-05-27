e5882cab5a345d59dbeebe4c58e70e69052dd808
ChristinaCattane <63089758+ChristinaCattane@users.noreply.github.com>
2020-05-27 17:56:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const formElement = document.querySelector(".form__popup-form");
-const nameInput = formElement.querySelector(".form__input_name");
+const modalElement = document.querySelector(".modal__popup-form");
+const nameInput = formElement.querySelector(".modal__input_name");
 const nameOutput = document.querySelector(".profile__name");
-const jobInput = formElement.querySelector(".form__input_job");
+const jobInput = formElement.querySelector(".modal__input_job");
 const jobOutput = document.querySelector(".profile__job");
 const editBtn = document.querySelector(".profile__edit-button");
-const closeBtn = document.querySelector(".form__close-button");
-const form = document.querySelector(".form__container");
+const closeBtn = document.querySelector(".modal__close-button");
+const modal = document.querySelector(".modal__container");
 
 
 function toggle() {
-    form.classList.toggle("form__container_active");
+    form.classList.toggle("modal__container_active");
 }
 
 function formSubmitHandler(evt) {
