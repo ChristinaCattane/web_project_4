4732cf24768ed39ef8a33672883b425593a8f6c8
ChristinaCattane <63089758+ChristinaCattane@users.noreply.github.com>
2020-05-27 21:06:07 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const modalElement = document.querySelector(".modal__popup-form");
-const nameInput = formElement.querySelector(".modal__input_name");
-const nameOutput = document.querySelector(".profile__name");
-const jobInput = formElement.querySelector(".modal__input_job");
-const jobOutput = document.querySelector(".profile__job");
-const editBtn = document.querySelector(".profile__edit-button");
-const closeBtn = document.querySelector(".modal__close-button");
-const modal = document.querySelector(".modal__container");
+const editButton = document.querySelector(".profile__edit-button");
+const modalCloseButton = document.querySelector(".modal__close-button");
+const modal = document.querySelector(".modal");
+const form = document.querySelector(".modal__popup-form");
+const inputName = document.querySelector(".modal__input_name");
+const inputJob = document.querySelector(".modal__input_job");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
 
-
-function toggle() {
-    form.classList.toggle("modal__container_active");
+function toggleModul() {
+    modal.classList.toggle("modal__visible");
 }
 
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    nameOutput.textContent = nameInput.value;
-    jobOutput.textContent = jobInput.value;
-    toggle()
-}
 
-formElement.addEventListener("submit", formSubmitHandler);
-editBtn.addEventListener("click", toggle);
-closeBtn.addEventListener("click", toggle);
+editButton.addEventListener("click", toggleModul);
+
+modalCloseButton.addEventListener("click", toggleModul);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    profileName.textContent = inputName.value
+    profileJob.textContent = inputJob.value
+
+})
+
+
 
 
 
