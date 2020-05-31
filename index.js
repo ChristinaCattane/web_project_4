003ae896ad59bba3f58d8ae7bdceb8a8c22191c9
ChristinaCattane <63089758+ChristinaCattane@users.noreply.github.com>
2020-05-31 19:12:20 -0400

    Add files via upload
    final commit for second submission

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const editButton = document.querySelector(".profile__edit-button");
 const modalCloseButton = document.querySelector(".modal__close-button");
 const modal = document.querySelector(".modal");
 const form = document.querySelector(".modal__popup-form");
-const inputName = document.querySelector(".modal__input_name");
-const inputJob = document.querySelector(".modal__input_job");
+const inputName = document.querySelector(".modal__input_type_name");
+const inputJob = document.querySelector(".modal__input_type_job");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
+const save = document.querySelector(".modal__button")
 
-function toggleModul() {
-    modal.classList.toggle("modal__visible");
+
+function toggleModal() {
+    modal.classList.toggle("modal-visible");
 }
-
-
-editButton.addEventListener("click", toggleModul);
-
-modalCloseButton.addEventListener("click", toggleModul);
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -23,6 +19,17 @@ form.addEventListener("submit", (e) => {
     profileJob.textContent = inputJob.value
 
 })
+function updateProfile(e) {
+    e.preventDefault();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    toggleModal();
+}
+save.addEventListener("click", toggleModal);
+editButton.addEventListener("click", toggleModal);
+modalCloseButton.addEventListener("click", toggleModal);
+
+
 
 
 
