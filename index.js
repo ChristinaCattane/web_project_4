35e848cfbe07581f881bd4a8ee54b7eb0a87df95
ChristinaCattane <63089758+ChristinaCattane@users.noreply.github.com>
2020-06-14 19:11:20 -0400

    Add files via upload
    image cards js fixed

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const cardTemplate = document.querySelector('.card-template').content.querySelec
 
 
 const createCard = (data) => {
+    
     const cardElement = cardTemplate.cloneNode(true);
 
     const cardTitle = cardElement.querySelector('.element__title');
@@ -69,6 +70,8 @@ const createCard = (data) => {
   
   cardTitle.textContent = data.name;
   cardImage.style.backgroundImage = `url(${data.link})`;
+  cardImage.style.backgroundSize = "cover";
+  cardImage.style.minHeight = "282px";
   
   cardLikeButton.addEventListener('click', () => {
     //changeHeartColor()
@@ -100,6 +103,112 @@ initialCards.forEach((data) => {
 })
 
 
+/*const initialCards = [
+  {
+      name: "Oregon Night Sky",
+      link: "./images/oregon-sky.jpg"
+  },
+  {
+      name: "Multnomah Falls, Oregon",
+      link: "./images/multnomah-falls.jpg"
+  },
+  {
+      name: "Mt. Hood, Oregon",
+      link: "./images/mt-hood.jpg"
+  },
+  {
+      name: "Haystack Rock, Oregon",
+      link: "./images/haystack-rock.jpg"
+  },
+  {
+      name: "Ecola State Park, Oregon",
+      link: "./images/ecola-park.jpg"
+  },
+  {
+      name: "Crater Lake, Oregon",
+      link: "./images/crater-lake.jpg"
+  }
+];
+
+//open full image/////////////////////////////////
+const popupFullImage = document.querySelector(".popup_type_image");
+const buttonCloseImage = popupFullImage.querySelector(".button__close");
+const popupImage = document.querySelector(".popup__image");
+const popupCaption = document.querySelector(".popup__caption");
+
+function fullImage(name, link) {
+  popupFullImage.classList.toggle("popup_active");
+  popupImage.src = link;
+  popupImage.alt = name;
+  popupCaption.textContent = name;
+}
+
+//add new card////////////////////////////////////
+const galleryTemplate = document.querySelector(".gallery__template").content.querySelector(".gallery__item");
+
+const createCard= (name, link) => {
+  const galleryElement = galleryTemplate.cloneNode(true);
+  const galleryImage = galleryElement.querySelector(".gallery__image");
+  const buttonRemove = galleryElement.querySelector(".button__remove");
+  const galleryTitle = galleryElement.querySelector(".gallery__title");
+  const buttonLike = galleryElement.querySelector(".button__like");
+
+  galleryTitle.textContent = name;
+  galleryImage.style.background = `url(${link})`;
+  galleryImage.style.backgroundSize = "cover";
+  galleryImage.style.minHeight = "282px";
+
+  buttonLike.addEventListener("click", (evt) => {
+    function changeHeartColor() {
+      buttonLike.classList.toggle("button__like_activated");
+    }
+    changeHeartColor();
+  });
+  buttonRemove.addEventListener("click", (evt) => {
+    evt.target.closest(".gallery__item").remove();
+  });
+  galleryImage.addEventListener("click", (evt) => {
+    fullImage(name, link);
+    galleryImage.addEventListener("click", fullImage);
+    buttonCloseImage.addEventListener("click", fullImage);
+
+  });
+
+  return galleryElement;
+};
+const galleryItems = document.querySelector(".gallery__items");
+
+const renderCard = (name, link) => {
+  galleryItems.prepend(createCard(name, link));
+};
+
+initialCards.forEach((data) => {
+  renderCard(data.name, data.link);
+});
+//Add button popup////////////////////////////////////////////////////
+
+const buttonAdd = document.querySelector(".button__add");
+const popupAdd = document.querySelector(".popup_type_add-button");
+const buttonCloseAdd = popupAdd.querySelector(".button__close");
+const formAdd = document.querySelector(".popup__form_type_add-button");
+const buttonCreate = document.querySelector(".button__submit_create");
+const inputTitle = document.querySelector(".popup__input_title");
+const inputImageLink = document.querySelector(".popup__input_image-link");
+
+function popUnpopAdd () {
+  popupAdd.classList.toggle("popup_active");
+  inputTitle.placeholder = "Image Title";
+  inputImageLink.placeholder = "Image Link";
+}
+buttonAdd.addEventListener("click", popUnpopAdd);
+buttonCloseAdd.addEventListener("click", popUnpopAdd);
+
+formAdd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  renderCard(inputTitle.value, inputImageLink.value);
+  popUnpopAdd ();
+});
+*/
 
 
 
