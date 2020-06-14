91031b789a983ae80360c556fd097978f1adf7c0
ChristinaCattane <63089758+ChristinaCattane@users.noreply.github.com>
2020-06-14 17:41:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const profileJob = document.querySelector(".profile__job");
 const save = document.querySelector(".modal__button")
 
 
+
 function toggleModal() {
     modal.classList.toggle("modal_visible");
     inputName.value = profileName.textContent
@@ -25,6 +26,78 @@ form.addEventListener("submit", (e) => {
 editButton.addEventListener("click", toggleModal);
 modalCloseButton.addEventListener("click", toggleModal);
 
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanois National Park",
+        link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+
+const cardTemplate = document.querySelector('.card-template').content.querySelector('.element');
+
+
+
+const createCard = (data) => {
+    const cardElement =cardTemplate.clonenode(true);
+
+    const cardTitle = cardElement.querySelector('element__title');
+    const cardImage = cardElement.querySelector('element__img');
+    const cardLikeButton = cardElement.querySelector('element__heart');
+    const cardRemoveButton =cardElement.querySelector('element__remove-button');
+  
+  
+  cardTitle.textContent = data.name;
+  cardImage.stye.backgroundImage = `url(${data.link})`;
+  
+  cardLikeButton.addEventListener('click', () => {
+    //changeHeartColor()
+  })
+  
+  cardRemoveButton.addEventListener('click', () => {
+      //RemoveCard
+  })
+  
+  cardImage.addEventListener('click', () => {
+      //openModul
+  })
+  
+  return cardElement;
+}
+
+const list = document.querySelector('.elements');
+
+
+const renderCard = (data) => {
+    list.prepend(createCard(data));
+}
+
+
+
+initialCards.forEach((data) => {
+    renderCard(data);
+
+})
 
 
 
